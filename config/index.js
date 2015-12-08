8a4cc9541dd97bbfee46bc5b26253a149642b589
Juan La Jara <wxomb@Juans-MacBook.local>
2015-12-08 17:46:17 -0500

    fix test publicHostname domain

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports =
 , emailing:
   { apiKey         : process.env.TELLER_DEV_ENV ? process.env.TELLER_DEV_EMAILING_APIKEY : process.env.TELLER_EMAILING_APIKEY
   , domain         : process.env.TELLER_DEV_ENV ? process.env.TELLER_DEV_EMAILING_DOMAIN : process.env.TELLER_EMAILING_DOMAIN
-  , publicHostname : 'wxo.me'
+  , publicHostname : 'juanlajara.com'
   }
 , express :
   { port: 5000 }
